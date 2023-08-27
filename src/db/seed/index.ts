81724ed50f27d69792eb6be15ea36e9751427dd2
darallium <46854880+darallium@users.noreply.github.com>
2023-08-27 12:54:50 +0900

    Update index.ts

--- src/db/seed/index.ts
+++ src/db/seed/index.ts
@@ -2,49 +2,49 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const insertUsers = async (
-  data: Prisma.Enumerable<Prisma.UserCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.UserCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.user.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to users table`,
+    `successfully inserted records of ${createMany.count} to users table`
   );
 };
 
 export const insertPosts = async (
-  data: Prisma.Enumerable<Prisma.PostCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.PostCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.post.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to posts table`,
+    `successfully inserted records of ${createMany.count} to posts table`
   );
 };
 
 export const insertLikes = async (
-  data: Prisma.Enumerable<Prisma.LikeCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.LikeCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.like.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to likes table`,
+    `successfully inserted records of ${createMany.count} to likes table`
   );
 };
 
 export const insertFollows = async (
-  data: Prisma.Enumerable<Prisma.FollowCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.FollowCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.follow.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to follows table`,
+    `successfully inserted records of ${createMany.count} to follows table`
   );
 };
