5cfefbb2c4d1f8b10736e7a53e5c77001a776b67
darallium <annihilation0504@gmail.com>
2023-08-29 12:33:56 +0000

    format

--- src/db/seed/index.ts
+++ src/db/seed/index.ts
@@ -1,49 +1,49 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import {Prisma, PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const insertUsers = async (
-  data: Prisma.Enumerable<Prisma.UserCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.UserCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.user.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to user table`,
+    `successfully inserted records of ${createMany.count} to user table`
   );
 };
 
 export const insertPosts = async (
-  data: Prisma.Enumerable<Prisma.PostCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.PostCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.post.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to post table`,
+    `successfully inserted records of ${createMany.count} to post table`
   );
 };
 
 export const insertLikes = async (
-  data: Prisma.Enumerable<Prisma.LikeCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.LikeCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.like.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to like table`,
+    `successfully inserted records of ${createMany.count} to like table`
   );
 };
 export const insertFollows = async (
-  data: Prisma.Enumerable<Prisma.FollowCreateManyInput>,
+  data: Prisma.Enumerable<Prisma.FollowCreateManyInput>
 ): Promise<void> => {
   const createMany = await prisma.follow.createMany({
     data,
     skipDuplicates: true,
   });
   console.log(
-    `successfully inserted records of ${createMany.count} to follow table`,
+    `successfully inserted records of ${createMany.count} to follow table`
   );
 };
